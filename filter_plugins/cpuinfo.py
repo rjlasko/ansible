0c1262127ef326e7bb53b26de378ec9f996eaac7
Robert Lasko <robert.j.lasko@gmail.com>
2021-02-06 22:44:51 -0500

    Add support specifying friendly cpu ids, for limiting the LXC container

--- filter_plugins/cpuinfo.py
+++ filter_plugins/cpuinfo.py
@@ -5,74 +5,76 @@ from collections import defaultdict
 
 
 class FilterModule(object):
-	def filters(self):
-		return {
-			'parse' : self.parseCpuInfo,
-			'idMap' : self.getIdMap,
-			'vcpuMap' : self.getVcpuMap
-		}
+    def filters(self):
+        return {
+            'parse' : self.parseCpuInfo,
+            'idMap' : self.getIdMap,
+            'vcpuMap' : self.getVcpuMap,
+            'nativeProcessorIds' : self.asNative
+        }
 
-	def parseCpuInfo(self, proc_cpuinfo):
-		numericRegex = r'^[+-]{0,1}((\d*\.)|\d*)\d+$'
+    def parseCpuInfo(self, proc_cpuinfo):
+        numericRegex = r'^[+-]{0,1}((\d*\.)|\d*)\d+$'
+        proc = dict()
+        cpuinfo = list()
+        for line in proc_cpuinfo:
+            line = line.strip()
+            if line:
+                keyval = [x.strip() for x in line.split(':')]
+                if not keyval[1]:
+                    value = None
+                elif keyval[1].isdigit():
+                    value = int(keyval[1])
+                elif re.match(numericRegex, keyval[1]):
+                    value = float(keyval[1])
+                elif keyval[1].lower() in ('true', 'yes', 'on'):
+                    value = True
+                elif keyval[1].lower() in ('false', 'no', 'off'):
+                    value = False
+                else:
+                    value = keyval[1]
 
-		proc = dict()
-		cpuinfo = list()
-		# cpuinfo.append(proc)
-		for line in proc_cpuinfo:
-			line = line.strip()
-			if line:
-				keyval = [x.strip() for x in line.split(':')]
-				if not keyval[1]:
-					value = None
-				elif keyval[1].isdigit():
-					value = int(keyval[1])
-				elif re.match(numericRegex, keyval[1]):
-					value = float(keyval[1])
-				elif keyval[1].lower() in ('true', 'yes', 'on'):
-					value = True
-				elif keyval[1].lower() in ('false', 'no', 'off'):
-					value = False
-				else:
-					value = keyval[1]
+                proc[keyval[0]] = value
+            elif proc:
+                cpuinfo.append(proc)
+                proc = dict()
+        # add last if populated
+        if proc:
+            cpuinfo.append(proc)
+        return cpuinfo
 
-				proc[keyval[0]] = value
-			elif proc:
-				cpuinfo.append(proc)
-				proc = dict()
-		# remove empty processor
-		if proc:
-			cpuinfo.append(proc)
-		return cpuinfo
+    def getCoreIdDict(self, cpuinfo):
+        ret = defaultdict(list)
+        for processor in cpuinfo:
+            ret[processor['core id']].append(processor)
+        return ret
 
-	def getCoreIdDict(self, cpuinfo):
-		ret = defaultdict(list)
-		for processor in cpuinfo:
-			ret[processor['core id']].append(processor)
-		return ret
+    def getProcIdsByCoreId(self, core_id, core_id_dict):
+        processors = core_id_dict[core_id]
+        return [x['processor'] for x in core_id_dict[core_id]]
 
-	def getProcIdsByCoreId(self, core_id, core_id_dict):
-		processors = core_id_dict[core_id]
-		return [x['processor'] for x in core_id_dict[core_id]]
+    def getIdMap(self, proc_cpuinfo):
+        core_id_dict = self.getCoreIdDict(self.parseCpuInfo(proc_cpuinfo))
+        core_ids = core_id_dict.keys()
+        idmap = dict()
+        for core_id in core_ids:
+            idmap[core_id] = self.getProcIdsByCoreId(core_id, core_id_dict)
+        return idmap
 
-	def getIdMap(self, proc_cpuinfo):
-		core_id_dict = self.getCoreIdDict(self.parseCpuInfo(proc_cpuinfo))
-		core_ids = core_id_dict.keys()
-		idmap = dict()
-		for core_id in core_ids:
-			idmap[core_id] = self.getProcIdsByCoreId(core_id, core_id_dict)
-		return idmap
+    def getVcpuMap(self, proc_cpuinfo):
+        idmap = self.getIdMap(proc_cpuinfo)
+        vcpuMap = dict()
+        natural_id = 0
+        for core_id in sorted(idmap.keys()):
+            for vcpu_id in sorted(idmap[core_id]):
+                vcpuMap[natural_id] = vcpu_id
+                natural_id += 1
+        return vcpuMap
 
-	def getVcpuMap(self, proc_cpuinfo):
-		idmap = self.getIdMap(proc_cpuinfo)
-		vcpuMap = dict()
-		natural_id = 0
-		for core_id in sorted(idmap.keys()):
-			for vcpu_id in sorted(idmap[core_id]):
-				vcpuMap[natural_id] = vcpu_id
-				natural_id += 1
-		return vcpuMap
+    def asNative(self, natural_ids, proc_cpuinfo):
+        vcpuMap = self.getVcpuMap(proc_cpuinfo)
+        return ','.join([str(vcpuMap[i]) for i in natural_ids])
 
-import sys
-# TODO: remove these
-import json
-print(json.dumps(FilterModule().getVcpuMap(sys.stdin.readlines())))
+# XXX: for testing only
+# import sys, json
+# print(json.dumps(FilterModule().getVcpuMap(sys.stdin.readlines())))
